93c320092d711d13b4d306155f2f26e00cdc704f
Edon Sekiraqa <edon@frakton.com>
2019-08-25 15:09:59 +0200

    Don't allow the localhook to be accesible externaly

--- chrome-extension/js/main.js
+++ chrome-extension/js/main.js
@@ -14,15 +14,30 @@ $("#start_button").click(function(){
         alert("Please provide the port where local webserver is listening");
         return;
     }
-    $(this).html("...");
-    $(this).attr("disabled", true);
-    chrome.extension.sendMessage({ function: "startConnection", host: $("#host").val(), port: $("#host_port").val() }, (response) => {
-        if (response.code === 200){
-            window.close();
-        }else{
-            $(this).attr("disabled", false);
-            $(this).html("START");
-            alert("Could not create connection to server. Please restart the extension.");
+    const button = $(this);
+    button.html("...");
+    button.attr("disabled", true);
+    const checkUrl = 'http://myhook.io/url-check?url='+$("#host").val().trim();
+    $.get(checkUrl, function(response){
+        console.log(response.alive);
+        if (typeof response !== 'undefined' && 
+            typeof response.alive !== 'undefined' &&
+            response.alive === false){
+                chrome.extension.sendMessage({ function: "startConnection", host: $("#host").val(), port: $("#host_port").val() }, (response) => {
+                    if (typeof response !== 'undefined' && response.code === 200){
+                        window.close();
+                    }else{
+                        button.attr("disabled", false);
+                        button.html("START");
+                        alert("Could not create connection to server. Please restart the extension and check the internet connection.");
+                    }
+                });
+        }else {
+            button.attr("disabled", false);
+            button.html("START");
+            alert('Seems your localhook ('+$("#host").val().trim()+') is accessible from external network. Your localhook should ping to internal network.');
         }
+        
     });
+
 });
