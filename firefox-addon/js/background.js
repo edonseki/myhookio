48e25677c46ba8b45c4182095e20034efaed4f9e
Edon Sekiraqa <edon@frakton.com>
2019-09-01 21:26:34 +0200

    Clone the chrome extension for Firefox Addon and perform the required changes.

---
+++ firefox-addon/js/background.js
@@ -0,0 +1,67 @@
+let currentWindow = null;
+
+browser.browserAction.onClicked.addListener(function(tab) {
+    const width = 490;
+    const height= 712;
+    openWindow('main.html', width, height);
+});
+
+browser.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        if (typeof request.function !== 'undefined'){
+            functionCallHandler(request, sender, sendResponse);
+            return true;
+        }
+        return false;
+    }
+);
+
+const openWindow = (filename, width, height) => {
+    const left = parseInt((screen.width/2)-(width/2));
+    const top = parseInt((screen.height/2)-(height/2)); 
+    chrome.windows.create({
+        url: browser.runtime.getURL(filename),
+        type: "popup",
+        width: parseInt(width),
+        height: parseInt(height),
+        left: left, 
+        top: top
+    });
+};
+
+const functionCallHandler = (request, sender, sendResponse) =>{
+    switch (request.function) {
+        case 'startConnection':
+            const host = request.host;
+            const port = request.port;
+            startConnection(host, port, (response) => {
+                if(response === false || currentWindow !== null){
+                    return;
+                }
+                const width = window.screen.availWidth/1.08;
+                const height = window.screen.availHeight/1.3;
+                openWindow('workspace.html', parseInt(width), parseInt(height));
+                currentWindow = {};
+                sendResponse(response);
+            });
+            break;
+        case 'stopConnection':
+            stopConnection();
+            const width = 490;
+            const height= 712;
+            openWindow('main.html', width, height);
+            break;
+        case 'terminateTheExtension':
+            terminateTheExtension();
+            break;
+        case 'getData':
+            sendResponse(getConnectionDetails());
+            break;
+        case 'requestDetails':
+            sendResponse(requestHistory[request.request_id]);
+            break;
+        case 'clearRequestHistory':
+            clearRequestHistory(request, sendResponse);
+            break;
+    }
+}
---
+++ firefox-addon/js/logic.js
@@ -0,0 +1,130 @@
+let started = false;
+let startedAt = null;
+let socket  = null;
+let listeningUrl = null;
+let publicSubdomain = null;
+const wssUrl = 'http://myhook.io';
+let requestHistory = {};
+
+const getConnectionDetails = () => {
+    return {
+        listeningUrl    : listeningUrl,
+        publicSubdomain : publicSubdomain,
+        requestHistory  : requestHistory,
+    };
+};
+
+const connectionErrorCallback = () => {
+    browser.runtime.sendMessage({body: false, type: 'status'});
+}
+
+const startConnection = (listeningHost, listeningPort, sendResponse) => {
+    if (socket == null || !started) {
+        try {
+            socket = io.connect(wssUrl);
+            socket.on("disconnect", connectionErrorCallback);
+            socket.on("connect_failed", connectionErrorCallback);
+            socket.on("connect_error", connectionErrorCallback);
+            socket.on('onSubdomainPrepared', (subdomain) => {
+                started = true;
+                publicSubdomain = subdomain;
+                startedAt = new Date();
+                if (typeof sendResponse !== 'undefined') {
+                    sendResponse({
+                        code: 200
+                    });
+                }
+            });
+            socket.on('onRequest', handleRequest);
+            const port = listeningPort == '80' ? '' : ':'+listeningPort;
+            listeningUrl = 'http://'+listeningHost+port;
+        }catch (e){
+            sendResponse({});
+        }
+    }else{
+        sendResponse({});
+    }
+};
+
+const stopConnection = (sendResponse) => {
+    if (socket !== null && started){
+        socket.disconnect();
+        socket = null;
+        started = false;
+        listeningUrl = null;
+        publicSubdomain = null;
+        requestHistory = {};
+        if (typeof sendResponse !== 'undefined') {
+            sendResponse({
+                code: 200
+            });
+        }
+    }
+};
+
+const terminateTheExtension = (sendResponse) => {
+    currentWindow = null;
+    stopConnection();
+
+    if(typeof sendResponse !== 'undefined'){
+        sendResponse({status: 200});
+    }
+};
+
+const emitResponseToSocket = (requestId, response) => {
+    const unpreparedHeaders = response.getAllResponseHeaders().split("\r\n");
+    const headers = {};
+
+    for(let i=0; i<unpreparedHeaders.length; i++){
+        const currentHeaderString = unpreparedHeaders[i];
+        const splitIdx      = currentHeaderString.indexOf(':');
+        const headerKey     = currentHeaderString.substring(0, splitIdx);
+        const headerValue   = currentHeaderString.substring(splitIdx+2);
+        headerKey.trim().length == 0 || (headers[headerKey] = headerValue);
+    }
+
+    //TODO this should be as an extra setting for the workspace user. For now it will be always performed.
+    response.responseText = response.responseText.split(listeningUrl).join(publicSubdomain.substring(0, publicSubdomain.length-1));
+
+    const responseBody = {
+        id              : requestId,
+        headers         : headers,
+        response_text   : response.responseText,
+        status          : response.status,
+        status_text     : response.status_text
+    };
+
+    let duration = new Date().getTime()-requestHistory[requestId].local_date.getTime();
+    responseBody.duration = duration;
+    browser.runtime.sendMessage({body: responseBody, type: 'response'});
+
+    //append response to request
+    requestHistory[requestId].response = responseBody;
+
+    socket.emit('onResponse', responseBody);
+};
+
+const handleRequest = (request) => {
+    const url = listeningUrl + request.path;
+    request.local_date = new Date();
+    browser.runtime.sendMessage({body: request, type: 'request'});
+    requestHistory[request.id] = request;
+
+    $.ajax({
+        url: url,
+        headers: request.headers,
+        method: request.method,
+        data: request.body,
+        success: (a1,status,response) => {
+            emitResponseToSocket(request.id, response);
+        },
+        error: (response, status, error) => {
+            emitResponseToSocket(request.id, response);
+        }
+    });
+};
+
+const clearRequestHistory = (request, sendResponseCallback) => {
+    requestHistory = {};
+    sendResponseCallback({});
+};
